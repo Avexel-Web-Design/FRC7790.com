61b8dd4f813e2cbaaaf37502a1e403f09743dc0e
Ryan Latimer <rlatimer96@harborps.org>
2025-01-20 13:31:46 -0500

    Still a blank page.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Navbar from "./components/Navbar/Navbar";
+import Content from "./components/Content/Content";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Navbar />
+        <Header />
+        <Content />
+        <Footer />
+    </StrictMode>,
 );
