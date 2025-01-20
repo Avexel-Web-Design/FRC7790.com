596ba103a267ad5256a97a6382dedac4f7c10c1c
Ryan Latimer <rlatimer96@harborps.org>
2025-01-20 15:48:33 +0000

    Start Development

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () =>{
+    return (
+        <nav className ="navbar">
+            <ul>
+                <l1></l1>
+                <l2></l2>
+                <l3></l3>
+                <l4></l4>
+            </ul>
+        </nav>
+    )
+}
