d2e1a5366ce7f3835054d718ff413ff0fb781e3c
Ryan Latimer <rlatimer96@harborps.org>
2025-01-24 18:21:53 -0500

    Fix animation

--- public/pages/Robots/Robots.js
+++ public/pages/Robots/Robots.js
@@ -1,4 +1,4 @@
-//Animate the navbar when scrolling
+// Animate the navbar when scrolling
 let lastScrollTop = 0;
 const navbar = document.getElementById('navbar');
 const scrollThreshold = 10; // Adjust this value to set how soon the navbar disappears
@@ -15,31 +15,7 @@ window.addEventListener('scroll', () => {
   lastScrollTop = scrollTop;
 });
 
-//Code to affect when the different robots appear on the screen
-document.addEventListener('DOMContentLoaded', () => {
-  const robots = document.querySelectorAll('.riptide, .cobalt, .hungry');
-
-  const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.75
-  };
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        observer.unobserve(entry.target);
-      }
-    });
-  }, observerOptions);
-
-  robots.forEach(robot => {
-    robot.classList.add('robot');
-    observer.observe(robot);
-  });
-});
-
+// Code to affect when the different robots appear on the screen
 document.addEventListener('DOMContentLoaded', () => {
   const robots = document.querySelectorAll('.riptide, .cobalt, .hungry');
 
