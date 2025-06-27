61054b3c848d3c7e3faf8bc5c256721aa0115f36
Gavin Moceri <chmcs@proton.me>
2025-06-27 18:03:21 -0400

    login readable

--- src/components/pages/auth/Login.tsx
+++ src/components/pages/auth/Login.tsx
@@ -31,7 +31,7 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gray-50">
+    <div className="min-h-screen flex justify-center items-center bg-gray-500">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
